cfbce432dd56ae723c5292077b4eae7cf3380810
ericmbride <eric.mcbride@rewardstyle.com>
2016-08-20 14:48:58 -0500

    moved system info out of main

--- src/system/system.rs
+++ src/system/system.rs
@@ -1,6 +1,30 @@
 extern crate num_cpus;
 extern crate sys_info;
 
+#[derive(Debug)]
+pub struct SystemInformation {
+    num_cpus: usize,
+    processor_speed: u64,
+    os_type: String,
+    os_release: String,
+    disk_info_free: u64,
+    disk_info_total: u64,
+    hostname: String,
+}
+
+impl SystemInformation {
+    pub fn new() -> SystemInformation {
+        SystemInformation {
+            num_cpus: get_cpus(),
+            processor_speed: get_cpu_speed(),
+            os_type: get_os_type(),
+            os_release: get_os_release(),
+            disk_info_free: get_disk_info_free(),
+            disk_info_total: get_disk_info_total(),
+            hostname: get_hostname(),
+        }
+    }
+}
 pub fn get_cpus() -> usize {
     return num_cpus::get();
 }
